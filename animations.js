1e007fbedfae57ea0f4c5cc0a0643243c1b00510
rijhul-kaushal <rijhulkaushal36@gmail.com>
2025-04-29 16:53:20 +0530

    hover effects on product card

--- animations.js
+++ animations.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }, 300 * (index + 1));
     });
     
-    // Product card hover effects
+    // hover effects on product card 
     const productCards = document.querySelectorAll('.product-card');
     productCards.forEach(card => {
       card.addEventListener('mouseenter', () => {
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     
     // hover image effect instagram
-    
+
     const instagramImages = document.querySelectorAll('.instagram-image');
     instagramImages.forEach(image => {
       image.addEventListener('mouseenter', () => {
